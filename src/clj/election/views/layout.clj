0c2cd77cf4aa1dd8f7c7139a7af3fa408e9b698f
Rafael Caceres <rafael@taller.net.br>
2016-09-14 18:46:24 -0300

    Styles improvements to vote and election pages (#14)
    * Added some css improvements.
    * Added container to content.
    * Added logo to header.
    * Fixed flash messages css.

--- src/clj/election/views/layout.clj
+++ src/clj/election/views/layout.clj
@@ -17,6 +17,9 @@
     [:body {}
       [:div.header
         [:div.container
+          [:div.logo
+            [:img{:src "/images/agile-alliance.jpg"}]
+          ]
           [:ul.actions]
           [:div.session
             (if (nil? (:user session))
@@ -31,7 +34,9 @@
         :notice [:div.flash.notice message]
         [:div.flash]
       )
-      [:div.content content]
+      [:div.content
+        [:div.container content]
+      ]
       (page/include-js "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js")
       (map page/include-js (link/bundle-paths request ["application.js"]))
     ]
