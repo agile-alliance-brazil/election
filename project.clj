65c6561ea02598bb73c5ace5441e8e5121b9adf7
Hugo Corbucci <hugo.corbucci@gmail.com>
2016-09-16 01:07:21 -0400

    Upgrading postgres jdbc

--- project.clj
+++ project.clj
@@ -27,7 +27,7 @@
       [clj-http "3.2.0"]
       [hiccup "1.0.5"]
       [ragtime "0.6.3"]
-      [org.postgresql/postgresql "9.4.1209"]
+      [org.postgresql/postgresql "9.4.1210"]
       [com.mchange/c3p0 "0.9.5.2"]
       [honeysql "0.8.0"]
       [clj-time "0.12.0"]
