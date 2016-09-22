8f51121c42b80dbdd46b3f1bab7b304c447482fb
Hugo Corbucci <hugo.corbucci@gmail.com>
2016-09-21 21:25:58 -0700

    Adjusting for election data

---
+++ resources/migrations/0006-2017-2019-aab-election.up.sql
@@ -0,0 +1,74 @@
+INSERT INTO elections (
+  id, name, description, startdate, enddate, candidatestoelect, candidatestovoteon)
+VALUES (
+  1,
+  'Eleição da Agile Alliance Brazil para Board 2017-2019',
+  'Eleição para election is for the 2017-2019 board',
+  '2016-09-23 00:00:00-03',
+  '2016-10-07 23:59:59-03',
+  3,
+  3);
+
+INSERT INTO candidates (
+  id, electionid, fullname, minibio, email, votecount)
+VALUES (
+  1,
+  1,
+  'Guilherme Motta',
+  'Tive a oportunidade de ser membro do Brazilian Leadership Team (BLT) da Thoughtworks Brasil (board) e foi uma experiencia incrível, tanto pessoal quanto profissional. Acabei conhecendo e interagindo com profissionais extremamente experientes que me fizeram pensar fora da caixa. Desta mesma forma, vejo como uma grande oportunidade ter a chance de poder participar do Board da Agile Alliance Brazil, e acredito que seria mais uma maneira de poder contribuir e colaborar com mais ações para desenvolver profissionais, equipes e a comunidade ágil brasileira.',
+  'gfcmotta@gmail.com',
+  0);
+INSERT INTO candidates (
+  id, electionid, fullname, minibio, email, votecount)
+VALUES (
+  2,
+  1,
+  'Marlon Glaciano',
+  'Na minha experiência na Agile Brazil aprendi muito nos mais diversos aspectos, vi a conferência evoluir de um grupo para uma instituição, enxergo uma progressão na transparência nos processos e tenho a esperança de que a AAB pode fazer mais ainda.
+
+Em todas as minhas participações, eu faço questão de um valor: coragem. Do universo de pessoas que trabalharam comigo, há quem me odeie e quem adore meu estilo. Algo inegável, no entanto, é que eu ajo -- eu ativamente busco o que fazer e me envolvo com os trabalhos para tirá-los do papel.
+
+E é isso que eu quero levar para o board da AAB: ação. Quero tirar do papel o monte de ideias boas que ouço e experimentá-las, engajar as tantas pessoas da comunidade que, tanto querem se ajudar, que ativamente participam do agilidade.slack.com. E, claro, quero continuar contribuindo para o movimento ágil no Brasil de uma forma diferente do que venho fazendo, e aprender com o processo.',
+  'marlonglaciano@gmail.com',
+  0);
+INSERT INTO candidates (
+  id, electionid, fullname, minibio, email, votecount)
+VALUES (
+  3,
+  1,
+  'Cecilia Fernandes',
+  'De SP!',
+  'contato@cecifernandes.com',
+  0);
+INSERT INTO candidates (
+  id, electionid, fullname, minibio, email, votecount)
+VALUES (
+  4,
+  1,
+  'Ari do Amaral Torres Filho',
+  'Sou apaixonado por Agilidade e tenho procurado sempre poder fazer ações para que as empresas e pessoas gerem resultados masi eficazes. Minha motivação é poder trazer incentivos para o Brasil e para o Nordeste para fazer eventos de agilidade, usar o nome da Agile Alliance para propagar Agile de forma mais forte por aqui. Me engajar e me dedicar com todas as forças a algo que respito e vivo desde 2008, Agilidade.',
+  'ariamaralt@gmail.com',
+  0);
+INSERT INTO candidates (
+  id, electionid, fullname, minibio, email, votecount)
+VALUES (
+  5,
+  1,
+  'Magno de Santana Silva',
+  'Embora as equipes técnicas tenham avançado nos últimos anos com o uso de conceitos e práticas Ágeis, acredito que ainda há um abismo entre o pessoal
+técnico e o pessoal de negócio.
+
+Quero contribuir com meu conhecimento para diminuir o abismo existente entre essas áreas, promovendo eventos com maior
+foco na área de negócios, para que haja equilíbrio entre as partes. Aumentando, dessa forma, a entrega de software de valor baseado em conceitos e práticas do mundo Ágil, além de fortalecer
+a comunidade Ágil na região centro-oeste do país, principalmente no estado do Mato Grosso do Sul.',
+  'magnodesantana@gmail.com',
+  0);
+INSERT INTO candidates (
+  id, electionid, fullname, minibio, email, votecount)
+VALUES (
+  6,
+  1,
+  'Fabio Aguiar',
+  'Tenho atuado intensamente na comunidade ágil na região Norte do Brasil, através do tasafo.org, organizando eventos, facilitando aprendizado e palestras. Estou fortemente envolvido desde a criação do Tá safo!, um grupo originado em Belém no estado do Pará, cujo o objetivo é a disseminação de Tecnologias Abertas com Software Ágil, Fácil e Organizado, palavras chaves que deram origem a sigla que representa a comunidade, ou seja, Desenvolvimento Ágil com Tecnologias Abertas. Minha motivação é escalar essa atuação, atuando fortemente junto a todas as comunidades a nível nacional compartilhando um pouco da minha experiência nesses 10 anos de Tá safo!',
+  'fabioaguiar@gmail.com',
+  0);
