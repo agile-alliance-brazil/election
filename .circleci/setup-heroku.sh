a2a91a2059fd5b5ec870d9afdb70e7adf7e0bb9e
Hugo Corbucci <hugo.corbucci@gmail.com>
2017-07-23 20:25:31 -0400

    Fixing heroku setup

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -4,8 +4,8 @@ set -e
 git remote add heroku https://git.heroku.com/aab-election.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 mkdir -p /usr/local/lib /usr/local/bin
-tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
-ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+tar -xvzf heroku-linux-amd64.tar.gz
+./heroku/install
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
