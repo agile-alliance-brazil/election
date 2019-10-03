b5b17d18f8b9ed761864eed7971910286fc0e9fb
Kineolyan <kineolyan@gmail.com>
2019-10-03 22:17:43 +0200

    Add tests on email validation and invalid filtering.

--- src/clj/election/controllers/elections.clj
+++ src/clj/election/controllers/elections.clj
@@ -61,7 +61,7 @@
 
 ; RegExp from http://emailregex.com/
 (def email-pattern #"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])")
-(def is-valid-email? [value]
+(defn is-valid-email? [value]
   (re-matches email-pattern value))
 (defn get-invalid-mails [voters]
   (filter
@@ -74,7 +74,7 @@
     (if (auth/can-register-voters? election user)
       (let [voters (voters-from voters-file)
             invalid-voters (get-invalid-mails voters)]
-        (if (empty? invalid-voters))
+        (if (empty? invalid-voters)
           (let [added-voters-count (voters/register-voters (:id election) voters)]
             (if added-voters-count
               (assoc response :flash {:type :notice :message (i18n/t request :elections/new-voters-registered added-voters-count)})
@@ -85,7 +85,11 @@
             :flash 
             {
               :type :error 
-              :message (i18n/t request :elections/invalid-voter-mails (count invalid-voters))}))
+              :message (i18n/t request :elections/invalid-voter-mails (count invalid-voters))
+            }
+          )
+        )
+      )
       (assoc response :flash {:type :error :message (i18n/t request :forbidden)})
     )
   )
--- test/clj/election/controllers/elections_test.clj
+++ test/clj/election/controllers/elections_test.clj
@@ -6,5 +6,47 @@
 (deftest email-validation
   (testing "accepts gmail email"
     (let [result (e/is-valid-email? "someone@gmail.com")]
-      (is (= true result))
-    )))
+      (is result)
+    )
+  )
+  (testing "accepts email with dots"
+    (let [result (e/is-valid-email? "a.b.c@yahoo.com")]
+      (is result)
+    )
+  )
+  (testing "accepts special gmail with +"
+    (let [result (e/is-valid-email? "some+one@gmail.com")]
+      (is result)
+    )
+  )
+  (testing "rejects email without @"
+    (let [result (e/is-valid-email? "agmail.com")]
+      (is (not result))
+    )
+  )
+  (testing "rejects email without domain"
+    (let [result (e/is-valid-email? "some@gmail")]
+      (is (not result))
+    )
+  )
+)
+
+(deftest invalid-mail-filtering
+  (testing "returns empty list if all is good"
+    (let [voters [[1 "a@gmail.com" "a"]
+                  [2 "b@outlook.com" "b"]
+                  [3 "c@protonmail.com", "c"]]
+          invalid (e/get-invalid-mails voters)]
+      (is (empty? invalid))
+    )
+  )
+  (testing "returns the wrong voters if any"
+    (let [voters [[1 "bad.mail.com"]
+                  [2 "b@outlook.com"]
+                  [3 "not@yahoo"]]
+          invalid (e/get-invalid-mails voters)]
+      (is (= invalid [[1 "bad.mail.com"]
+                      [3 "not@yahoo"]]))
+    )
+  )
+)
